bb0a602e4ffb5009213057c878fe99054c6e7a24
240421 <240421@vutbr.cz>
2025-03-26 14:58:29 +0100

    Lab4 commit

---
+++ include/KeyboardControl.hpp
@@ -0,0 +1,24 @@
+#ifndef KEYBOARDCONTROL
+#define KEYBOARDCONTROL
+
+#include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/twist.hpp"
+
+#include <termios.h>
+
+class KeyboardControlNode : public rclcpp::Node 
+{
+public:
+    KeyboardControlNode();
+    ~KeyboardControlNode();
+
+private:
+    void timerCallback();
+
+    //Publishers
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr twist_publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    struct termios old_termios_;
+};
+
+#endif // KEYBOARDCONTROL
