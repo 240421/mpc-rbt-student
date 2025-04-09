8baabca7df8b3dd05d96e5ffd74b3b844dee19e9
240421 <240421@vutbr.cz>
2025-04-09 14:17:32 +0200

    rviz - funkcne

--- launch/solution.launch.py
+++ launch/solution.launch.py
@@ -2,22 +2,17 @@ import os
 from launch_ros.actions import Node
 from launch import LaunchDescription
 from ament_index_python.packages import get_package_share_directory
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
     package_dir = get_package_share_directory('mpc_rbt_student')
     rviz_config_path = os.path.join(package_dir, 'rviz', 'config.rviz')
+    simulator_launch = os.path.join(get_package_share_directory('mpc_rbt_simulator'), 'launch', 'simulation.launch.py')
 
     
     return LaunchDescription([
-        # Launch localization node (modify node name & parameters as needed)
-        Node(
-            package='mpc_rbt_student',
-            executable='localization',
-            name='localization',
-            output='screen'
-        ),
-
         # Launch RViz2 with configuration file
         Node(
             package='rviz2',
@@ -25,6 +20,30 @@ def generate_launch_description():
             name='rviz2',
             output='screen',
             arguments=['-d', rviz_config_path]
+        ),
+
+        TimerAction(
+            period = 3.0,
+            actions = [
+                IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(simulator_launch)
+            )
+            ] 
+        ),
+
+        # Launch localization node (modify node name & parameters as needed)
+        TimerAction(
+        period = 10.0,
+        actions = [
+            Node(
+                package='mpc_rbt_student',
+                executable='localization',
+                name='localization',
+                output='screen'
+            )
+        ]
         )
+
+       
 
     ])
